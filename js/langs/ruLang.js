729e8d87587477aac588e00180c571cc9006be12
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-05-01 10:19:05 +0300

    feat: add starter js & css

---
+++ js/langs/ruLang.js
@@ -0,0 +1,4 @@
+// const ruAlphabetLine1 = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// const ruAlphabetLine2 = [2];
+
+export default ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+// import * as line1 from './langs/ruLang';
+
+const body = document.querySelector('body');
+
+body.insertAdjacentHTML(
+  'beforebegin',
+  '<div class="main-wrapper">keyboard</div>'
+);
+
+// ruAlphabetLine1.forEach((el) => console.log(el));
+
+// console.log(line1);
+~
