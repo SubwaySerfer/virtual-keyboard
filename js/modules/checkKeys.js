378bb3a056097c86f86068494915504297634464
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-05-01 22:55:00 +0300

    feat: add gh-pages branch

--- js/modules/checkKeys.js
+++ js/modules/checkKeys.js
@@ -5,7 +5,7 @@ function checkKeys(content, textArea) {
   content.addEventListener('click', () => {
     if (specialSymbols.includes(event.target.textContent.toUpperCase())) {
       otherKeys(event.target.textContent.toUpperCase(), textArea);
-      //TODO: нужно поменять синтаксис. пока что убирает клики по ul так
+      // TODO: нужно поменять синтаксис. пока что убирает клики по ul так
     } else if (event.target.textContent.length > 20) {
     } else {
       textArea.textContent += event.target.textContent;
